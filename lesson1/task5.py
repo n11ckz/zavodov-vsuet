244247c72bb0666dab64e4b49f8b77c79a5dc281
n11ckz <nickzavodovmain@gmail.com>
2023-09-06 23:01:01 +0300

    Update task5.py

--- lesson1/task5.py
+++ lesson1/task5.py
@@ -1,5 +1,5 @@
 def calculateExpression(number):
-    return number + number**2 + number**3 + number**4 + number**5
+    return number + number ** 2 + number ** 3 + number ** 4 + number ** 5
 
 number = 5
 print(calculateExpression(number))
