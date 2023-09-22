c2e882362122a2a1a77882c9fb19accef8cc5e1e
n11ckz <nickzavodovmain@gmail.com>
2023-09-23 00:24:07 +0300

    vsuet

--- lesson3/task7.py
+++ lesson3/task7.py
@@ -1,10 +1,10 @@
-def isLeapYear(year) -> bool:
+def isYearLeap(year) -> bool:
     if year % 4 == 0 or year % 400 == 0:
         return True
     return False
 
 currentYear = 2023
-if isLeapYear(currentYear):
+if isYearLeap(currentYear):
     print(f"{currentYear} - leap year")
 else:
     print(f"{currentYear} - non-leap year")
