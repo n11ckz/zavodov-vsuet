677cc3a16f616534e01e42afb9b4ddd1e1a7304f
nick <126013251+n11ckz@users.noreply.github.com>
2023-11-08 02:23:42 +0300

    vuset

--- Lesson1/Task1.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD:lesson1/task1.py
-message: str = "Курс основы программирования начался"
-print(message)
-=======
-message = "Курс основы программирования начался"
-print(message)
->>>>>>> 18eb74cab4fcdb36a840438020ec9e0028801e35:Lesson1/Task1.py
